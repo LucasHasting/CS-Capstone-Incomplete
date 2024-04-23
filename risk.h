2fab0bfad083cdae6122b24e69f5f1ba3669c27b
Diksha Chottani <dichot@cs.csis.work>
2024-04-23 16:56:17 -0500

    Risk class inital implementation

---
+++ risk.h
@@ -0,0 +1,24 @@
+#include<string>
+#include"DatabaseConnection.h"
+
+class risk
+{
+	private:
+		std::string ID;
+		std::string closeDate;
+		std::string openDate;
+		std::string notes;
+		std::string status;
+		std::string owner;
+		int likelihoodRank;
+		int impactRank;
+		std::string longDescription;
+		std::string shortDescription;
+		DatabaseConnection connection;
+ 
+	public: 
+		Risk(std::string ID,std::string shortDescription,std::string longDescription,int likelihoodRank,int impactRank,std::string owner,std::string status,std::string notes,std::string openDate,std::string closeDate);
+		bool saveRisk();
+		bool vaidateID();
+		bool formatDate();
+}
