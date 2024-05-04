6c52f38f749996400d3a04e96e3e6fb089add90e
Braden Harris <bharris13@una.edu>
2024-05-03 22:16:18 -0500

    Upload files to 'src/model'

---
+++ src/model/DatabaseConnection.h
@@ -0,0 +1,24 @@
+#include "Risk.h"
+#include "User.h"
+#include <Wt/Auth/HashFunction.h>
+#include <Wt/Dbo/Dbo.h>
+#include <Wt/Dbo/backend/MySQL.h>
+
+#ifndef DBC
+#define DBC
+
+class DatabaseConnection {
+private:
+  Wt::Dbo::backend::MySQL *connection;
+  Wt::Dbo::Session session;
+  Wt::Auth::MD5HashFunction hashFunction;
+
+public:
+  DatabaseConnection();
+  bool addUser(std::unique_ptr<User>);
+  std::unique_ptr<User> authenticateUser(std::string, std::string);
+  std::unique_ptr<User> CreateUserFromDB(Wt::Dbo::ptr<User>::mutator);
+  bool addRisk(std::unique_ptr<Risk>);
+};
+
+#endif
---
+++ src/model/Risk.h
@@ -0,0 +1,42 @@
+#include <Wt/Dbo/Dbo.h>
+#include <string>
+
+#ifndef RISK
+#define RISK
+
+class Risk {
+private:
+  std::string ID;
+  std::string closeDate;
+  std::string openDate;
+  std::string notes;
+  std::string status;
+  std::string owner;
+  int likelihoodRank;
+  int impactRank;
+  std::string longDescription;
+  std::string shortDescription;
+
+public:
+  Risk(std::string ID, std::string shortDescription,
+       std::string longDescription, int likelihoodRank, int impactRank,
+       std::string owner, std::string status, std::string notes,
+       std::string openDate, std::string closeDate);
+  bool validateID();
+  bool formatDate();
+
+  template <class Action> void persist(Action &a) {
+    Wt::Dbo::field(a, ID, "ID");
+    Wt::Dbo::field(a,closeDate , "closeDate");
+    Wt::Dbo::field(a, openDate, "openDate");
+    Wt::Dbo::field(a, notes, "notes");
+    Wt::Dbo::field(a, status, "status");
+    Wt::Dbo::field(a, owner, "owner");
+    Wt::Dbo::field(a, likelihoodRank, "likelihoodRank");
+    Wt::Dbo::field(a, impactRank, "impactRank");
+    Wt::Dbo::field(a, longDescription, "longDescription");
+    Wt::Dbo::field(a, shortDescription, "shortDescription");
+  }
+};
+
+#endif 
