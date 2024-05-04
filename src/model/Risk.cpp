f9173cd0db036098bdaa7a293fd030a13f1df856
Braden Harris <bharris13@una.edu>
2024-05-04 13:21:48 -0500

    Update 'src/model/Risk.cpp'

--- src/model/Risk.cpp
+++ src/model/Risk.cpp
@@ -1,26 +1,63 @@
-#include "Risk.h"
+#include <Wt/Dbo/Dbo.h>
+#include <string>
 
-// Constructor
-Risk::Risk(std::string ID, std::string shortDescription,
-            std::string longDescription, int likelihoodRank, int impactRank,
-            std::string owner, std::string status, std::string notes,
-            std::string openDate, std::string closeDate)
-{
+#ifndef RISK
+#define RISK
 
-  this->ID = ID;
-  this->shortDescription = shortDescription;
-  this->longDescription = longDescription;
-  this->likelihoodRank = likelihoodRank;
-  this->impactRank = impactRank;
-  this->owner = owner;
-  this->status = status;
-  this->notes = notes;
-  this->openDate = openDate;
-  this->closeDate = closeDate;
+class Risk {
+private:
+  std::string ID;
+  std::string closeDate;
+  std::string openDate;
+  std::string notes;
+  std::string status;
+  std::string owner;
+  int likelihoodRank;
+  int impactRank;
+  std::string longDescription;
+  std::string shortDescription;
 
-}
+public:
+  Risk(std::string ID, std::string shortDescription,
+       std::string longDescription, int likelihoodRank, int impactRank,
+       std::string owner, std::string status, std::string notes,
+       std::string openDate, std::string closeDate);
+  bool validateID();
+  bool formatDate();
+  std::string getID();
+  std::string getCloseDate();
+  std::string getOpenDate();
+  std::string getNotes();
+  std::string getStatus();
+  std::string getOwner();
+  int getLikelihoodRank();
+  int getImpactRank();
+  std::string getLongDescription();
+  std::string getShortDescription();
+  void setID(std::string);
+  void setCloseDate(std::string);
+  void setOpenDate(std::string);
+  void setNotes(std::string);
+  void setStatus(std::string);
+  void setOwner(std::string);
+  void setLikelihoodRank(int);
+  void setImpactRank(int);
+  void setLongDescription(std::string);
+  void setShortDescription(std::string);
 
-// validateID() fucntion checks for the ID entered by the user with the database
-// to see if it actually exists.
 
-bool Risk::validateID() { return !ID.empty(); }
+  template <class Action> void persist(Action &a) {
+    Wt::Dbo::field(a, ID, "ID");
+    Wt::Dbo::field(a, closeDate, "closeDate");
+    Wt::Dbo::field(a, openDate, "openDate");
+    Wt::Dbo::field(a, notes, "notes");
+    Wt::Dbo::field(a, status, "status");
+    Wt::Dbo::field(a, owner, "owner");
+    Wt::Dbo::field(a, likelihoodRank, "likelihoodRank");
+    Wt::Dbo::field(a, impactRank, "impactRank");
+    Wt::Dbo::field(a, longDescription, "longDescription");
+    Wt::Dbo::field(a, shortDescription, "shortDescription");
+  }
+};
+
+#endif
