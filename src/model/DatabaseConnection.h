0b0715b910a67a9a1b0963a28b587b968f1910cb
Kent Terrell <keterr@cs.csis.work>
2024-05-04 13:56:16 -0500

    added createRiskFromDb and editRisk

--- src/model/DatabaseConnection.h
+++ src/model/DatabaseConnection.h
@@ -10,13 +10,16 @@ class DatabaseConnection {
 private:
   Wt::Dbo::backend::MySQL *connection;
   Wt::Dbo::Session session;
-  Wt::Auth::MD5HashFunction hashFunction;
+  Wt::Auth::BCryptHashFunction hashFunction;
 
 public:
   DatabaseConnection();
   bool addUser(std::unique_ptr<User>);
   std::unique_ptr<User> authenticateUser(std::string, std::string);
-  std::unique_ptr<User> CreateUserFromDB(Wt::Dbo::ptr<User>::mutator);
+  std::unique_ptr<User> CreateUserFromDB(Wt::Dbo::ptr<User>::mutator, bool, std::string);
+  std::unique_ptr<Risk> DatabaseConnection::CreateRiskFromDB(Wt::Dbo::ptr<Risk>::mutator r);
+  bool EditRisk(Wt::Dbo::ptr<Risk>::mutator r);
+  std::vector<std::unique_ptr<User>> select_all();
 };
 
 #endif
